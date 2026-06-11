e7a370951863420e656c4dc4c97308fec449a1eb
MaxPaolo660 <paolomax993@gmail.com>
2026-06-11 12:55:43 -0300

    ftyj

--- components/Cell.js
+++ components/Cell.js
@@ -1,16 +1,22 @@
 import React, { useMemo } from "react";
-import { TouchableOpacity, View, StyleSheet } from "react-native";
+import { TouchableOpacity, View, StyleSheet, Image } from "react-native";
 
 const tamanhos = {
-    small: 17,
-    medium: 23,
-    large: 32,
+    small: 23,
+    medium: 35,
+    large: 50,
 };
 const posicoes = [//posicoes "aleatorias"
     { x: -20, y: -20 },
     { x: 20, y: -20 },
     { x: 0, y: 20 },
 ];
+const imagens = {
+    "#8C3B2F": require("../assets/pieces/red.png"),
+    "#556B2F": require("../assets/pieces/green.png"),
+    "#4A6C8A": require("../assets/pieces/blue.png"),
+    "#B08A3E": require("../assets/pieces/yellow.png"),
+};
 export default function Cell({ cell, onPress }) {
     const positions = useMemo(() => {
         const embaralhadas = [...posicoes].sort(
@@ -22,22 +28,49 @@ export default function Cell({ cell, onPress }) {
             large: embaralhadas[2],
         };
     }, []);
-    function renderPeca(size, color) {//versao simplificada, testar para ver se funciona
-        const tamanho = tamanhos[size];//se nao funcionar ta no GitHub a versao antiga
+    function renderPeca(size, color) {
+        const tamanho = tamanhos[size];
         const pos = positions[size];
+        const left = 50 - tamanho / 2 + pos.x;
+        const top = 50 - tamanho / 2 + pos.y;
+        const offsets = [ // deslocamentos para formar o contorno
+            [-1, 0],
+            [1, 0],
+            [0, -1],
+            [0, 1],
+            [-1, -1],
+            [1, -1],
+            [-1, 1],
+            [1, 1],
+        ];
         return (
-            <View
-                style={{
-                    width: tamanho,
-                    height: tamanho,
-                    borderWidth: 5,
-                    backgroundColor: color,
-                    borderRadius: 100,
-                    position: "absolute",
-                    left: 50 - tamanho / 2 + pos.x,
-                    top: 50 - tamanho / 2 + pos.y,
-                }}
-            />
+            <>
+                {offsets.map(([dx, dy], index) => (
+                    <Image
+                        key={index}
+                        source={imagens[color]}
+                        tintColor="black"
+                        style={{
+                            width: tamanho,
+                            height: tamanho,
+                            position: "absolute",
+                            left: left + dx,
+                            top: top + dy,
+                            resizeMode: "contain",
+                        }}
+                    />
+                ))}
+                <Image
+                    source={imagens[color]}
+                    style={{
+                        width: tamanho,
+                        height: tamanho,
+                        position: "absolute",
+                        left, top,
+                        resizeMode: "contain",
+                    }}
+                />
+            </>
         );
     }
     return (
@@ -51,7 +84,6 @@ export default function Cell({ cell, onPress }) {
         </TouchableOpacity>
     );
 }
-
 const styles = StyleSheet.create({
     cell: {
         width: 100,
